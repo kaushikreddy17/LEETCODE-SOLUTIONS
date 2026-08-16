a028398f06ddf6db7d3dd9458674063c41629586
BURRI KAUSHIK REDDY <kaushikreddy8715@gmail.com>
2026-08-16 23:26:39 +0530

    Time: 585 ms (59.31%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -0,0 +1,8 @@
+SELECT employee_id, department_id
+FROM Employee
+where primary_flag = 'Y' 
+or employee_id in (
+        select employee_id 
+        from employee 
+        group by employee_id 
+        having count(*) = 1)
