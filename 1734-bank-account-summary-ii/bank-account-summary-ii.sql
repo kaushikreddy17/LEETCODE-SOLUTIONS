eba994079c43a91f0081242642ed686950080160
BURRI KAUSHIK REDDY <kaushikreddy8715@gmail.com>
2026-08-16 22:14:25 +0530

    Time: 798 ms (85.17%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1734-bank-account-summary-ii/bank-account-summary-ii.sql
@@ -0,0 +1,3 @@
+# Write your MySQL query statement below
+select u.name as NAME,sum(t.amount) as BALANCE from users u join transactions t
+on u.account=t.account group by u.name having sum(t.amount) >10000
