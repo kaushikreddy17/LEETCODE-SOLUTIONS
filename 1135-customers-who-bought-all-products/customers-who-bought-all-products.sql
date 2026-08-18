e46441ecc4ed8756e2569f4fc75cbdbef4f77316
BURRI KAUSHIK REDDY <kaushikreddy8715@gmail.com>
2026-08-18 22:09:29 +0530

    Time: 582 ms (71.97%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
@@ -0,0 +1,8 @@
+
+SELECT customer_id
+FROM Customer c
+JOIN Product p
+    ON c.product_key = p.product_key
+GROUP BY customer_id
+HAVING 
+    COUNT( DISTINCT c.product_key) = (SELECT COUNT(*) FROM Product);
