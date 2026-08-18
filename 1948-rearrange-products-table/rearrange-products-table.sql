25df9dfadef2f9162547760dc44b6682ce29dd41
BURRI KAUSHIK REDDY <kaushikreddy8715@gmail.com>
2026-08-18 19:40:28 +0530

    Time: 454 ms (32.43%) | Memory: 0B (100.00%) - LeetSync

--- 1948-rearrange-products-table/rearrange-products-table.sql
+++ 1948-rearrange-products-table/rearrange-products-table.sql
@@ -1,24 +1,7 @@
-# Write your MySQL query statement below
-SELECT product_id,
-       'store1' AS store,
-       store1 AS price
-  FROM Products
- WHERE store1 IS NOT NULL
-
- UNION
-
- SELECT product_id,
-       'store2' AS store,
-       store2 AS price
-  FROM Products
- WHERE store2 IS NOT NULL
-
- UNION
-
- SELECT product_id,
-       'store3' AS store,
-       store3 AS price
-  FROM Products
- WHERE store3 IS NOT NULL
-
- ORDER BY product_id, store;
+/* Write your T-SQL query statement below */
+SELECT product_id, store, price
+FROM Products
+UNPIVOT (
+    price FOR store IN (store1, store2, store3)
+) AS unpvt
+WHERE price IS NOT NULL;
